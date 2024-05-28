9b5f5cf7bf151a199be8ba78b6a20948f3cbf108
นายคเชนทร์ จันทเกษ <164714767+Kj-work68@users.noreply.github.com>
2024-05-28 16:09:05 +0700

    done

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 const arrows = document.querySelectorAll(".arrow");
 const movieLists = document.querySelectorAll(".movie-list");
 
-arrows.forEach((arrow, i)=>{
-    arrow.addEventListener("click",()=>{
-        const itemNumber = movieLists[i].querySelectorAll("img").length;
-        let clickCounter = 0;
-        arrow.addEventListener("click",()=>{
-            clickCounter++;
-            if(itemNumber - (5 + clickCounter) >= 0){
-            movieLists[i].style.transform = `translateX(${(movieLists[i].computedStyleMap().get("transform")[0].x.value)-250}px)`;
-            }
-        else{
-            movieLists[i].style.transform = "transformX(0)";
-        }
-        
-    });
+arrows.forEach((arrow, i) => {
+  const itemNumber = movieLists[i].querySelectorAll("img").length;
+  let clickCounter = 0;
+  arrow.addEventListener("click", () => {
+    clickCounter++;
+    if (itemNumber - (2 + clickCounter) >= 0) {
+      movieLists[i].style.transform = `translateX(${
+        movieLists[i].computedStyleMap().get("transform")[0].x.value - 250
+      }px)`;
+    } else {
+      movieLists[i].style.transform = "translateX(0)";
+      clickCounter = 0;
+    }
+  });
 
-    console.log(movieLists[i].querySelectorAll("img").length);
+  console.log(movieLists[i].querySelectorAll("img").length);
 });
+
+const ball = document.querySelector(".toggle-ball")
+const items = document.querySelectorAll(".container,.movie-list-title,.navbar-container,.sidebar,.left-menu-icon,.toggle")
+
+ball.addEventListener("click",()=>{
+  items.forEach((item)=>{
+    item.classList.toggle("active")
+  })
+  ball.classList.toggle("active")
+})
